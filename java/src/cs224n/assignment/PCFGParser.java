96066339e711dfbdb6aa0bc0f01fcd15a1e01d15
Colin Mayer <cmayer@stanford.edu>
2013-10-19 14:23:14 -0700

    Started parser

--- java/src/cs224n/assignment/PCFGParser.java
+++ java/src/cs224n/assignment/PCFGParser.java
@@ -1,7 +1,9 @@
 package cs224n.assignment;
 
+import cs224n.assignment.Grammar.UnaryRule;
 import cs224n.ling.Tree;
 import cs224n.util.CounterMap;
+import cs224n.util.Pair;
 
 import java.util.*;
 
@@ -12,7 +14,7 @@ public class PCFGParser implements Parser {
     private Grammar grammar;
     private Lexicon lexicon;
     CounterMap<List<String>,Tree<String>> knownParses;
-    CounterMap<Integer,String> spanToCategories;
+    CounterMap<Pair<Integer,Integer>,String> spanToCategories;
     
     public void train(List<Tree<String>> trainTrees) {
         // TODO: before you generate your grammar, the training trees
@@ -36,7 +38,38 @@ public class PCFGParser implements Parser {
     }
 
     public Tree<String> getBestParse(List<String> sentence) {
+    	spanToCategories = new CounterMap<Pair<Integer,Integer>,String>();
+    	
+    	//Get the lexicon tags
+    	Set<String> allTags = lexicon.getAllTags();
+    	
+    	//Get nonterminal counts
+    	for (int i=0; i < sentence.size(); i++){
+    		for (String tag : allTags)
+    			spanToCategories.setCount(getIntPair(i,i),tag,lexicon.scoreTagging(sentence.get(i), tag));
+    	}
+    	
+    	//Add unaries
+    	boolean added = true;
+    	while (added){
+    		for (int i=0; i < sentence.size(); i++){
+    			for (String tag : allTags){
+    				List<UnaryRule> unaryParents = grammar.getUnaryRulesByChild(tag);
+        			for (UnaryRule up : unaryParents){
+        				double p = up.getScore() * spanToCategories.getCount(getIntPair(i,i), up.parent);
+        				if (p > spanToCategories.getCount(getIntPair(i,i), tag)){
+        					spanToCategories.setCount(getIntPair(i,i), tag,p);
+        				}
+    			}
+    		}
+    	}
+
+    	
         // TODO: implement this method
         return null;
+    }
+    
+    private Pair<Integer,Integer> getIntPair(int a, int b){
+    	return new Pair<Integer,Integer>(a,b);
     }
 }
